ac79f99682b7caf90e0638ba05d14490ab2975b2
Nawzad75 <113518854+Nawzad75@users.noreply.github.com>
2023-10-18 09:04:35 +0200

    1

--- src/Screen/TaskScreen.tsx
+++ src/Screen/TaskScreen.tsx
@@ -1,15 +1,40 @@
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-
+import { TextInput } from 'react-native-paper';
 import { RootStackParamList } from '../Navigation/RootNavigator';
 
 type Props = NativeStackScreenProps<RootStackParamList, 'Task'>;
 
 export default function TaskScreen({ navigation }: Props) {
+  const [titel, setTitel] = React.useState('');
   return (
-    <View style={styles.container}>
-      <Text>Task Screen</Text>
+    <View style={{ backgroundColor: 'F2F2F2' }}>
+      <View
+        style={{
+          flexDirection: 'row',
+          justifyContent: 'center',
+          backgroundColor: 'FFFFFF',
+        }}
+      >
+        <Text
+          style={{
+            fontSize: 30,
+            paddingTop: 15,
+            alignContent: 'center',
+            height: 66,
+            backgroundColor: 'FFFFFF',
+          }}
+        >
+          Skapa en ny syssla
+        </Text>
+      </View>
+      <View style={{ height: 66 }}></View>
+      <TextInput
+        label="Titel"
+        value={titel}
+        onChangeText={(text) => setTitel(text)}
+      />
     </View>
   );
 }
