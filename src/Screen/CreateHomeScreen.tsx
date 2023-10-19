59db43d84ede4f75bc6e4a75ed198965b3f74786
Kayo-D <113675667+Kayo-D@users.noreply.github.com>
2023-10-19 15:01:49 +0200

    Update CreateHomeScreen.tsx

--- src/Screen/CreateHomeScreen.tsx
+++ src/Screen/CreateHomeScreen.tsx
@@ -4,14 +4,13 @@ import { StyleSheet, View, Text } from 'react-native';
 import { ProjectTheme } from '../../theme/theme';
 
 import { RootStackParamList } from '../Navigation/RootNavigator';
-import { TextInput } from 'react-native-paper';
+import { Button, TextInput } from 'react-native-paper';
 
 type Props = NativeStackScreenProps<RootStackParamList, 'CreateHome'>;
 
 export default function CreateHomeScreen({ navigation }: Props) {
   return (
     <View style={styles.container}>
-      {/* Custom header */}
       <View style={styles.header}>
         <Text style={styles.headerText}>Skapa Hushåll</Text>
       </View>
@@ -23,9 +22,19 @@ export default function CreateHomeScreen({ navigation }: Props) {
         placeholder="Ägare"
         style={styles.input}
       />
+      <View style={styles.footer}>
+        <Button style={styles.button} mode="contained" onPress={() => counter + 1}>
+          Spara
+        </Button>
+        <Button style={styles.button} mode="contained" onPress={() => counter - 1}>
+          Avbryt
+        </Button>
+      </View>
     </View>
   );
 }
+
+let counter = 0;
 
 const theme = ProjectTheme;
 
@@ -35,6 +44,8 @@ const styles = StyleSheet.create({
   },
   header: {
     padding: 10,
+    backgroundColor: theme.headerBackgroundColor,
+    elevation: 10,
   },
   headerText: {
     color: theme.headerTitleColor,
@@ -45,5 +56,29 @@ const styles = StyleSheet.create({
   input: {
     backgroundColor: theme.inputBackground,
     color: theme.inputPlaceholderColor,
+    borderRadius: 10,
+    height: 55,
+    width: 350,
+    marginLeft: 20,
+    marginTop: 15,
+    elevation: 5,
   },
+  footer: {
+    position: 'absolute',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    bottom: 0,
+    left: 0,
+    right: 0,
+  },
+  button:{
+    flex: 1, // Make buttons expand to fill the entire footer width
+    borderWidth: 1, // Add a thin border to separate the buttons
+    borderRadius: 0,
+    borderColor: '#ccc', // Border color
+    alignItems: 'center',
+    backgroundColor: 'white',
+    height: 80,
+  }
 });
