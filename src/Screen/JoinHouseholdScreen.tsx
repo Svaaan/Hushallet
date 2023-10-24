97652122683be477034ca75c21c3ef98dabe9a77
Honer Jamal <Honerjamal@gmail.com>
2023-10-25 01:25:43 +0200

    Added avatars and checkboxes

--- src/Screen/JoinHouseholdScreen.tsx
+++ src/Screen/JoinHouseholdScreen.tsx
@@ -1,26 +1,57 @@
-import { NativeStackScreenProps } from '@react-navigation/native-stack';
-import { View, TextInput } from 'react-native';
 import React, { useState } from 'react';
+import { View, TextInput, Image, TouchableOpacity, Text } from 'react-native';
+import { Checkbox } from 'react-native-paper';
+import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import { RootStackParamList } from '../Navigation/RootNavigator';
 import { ProjectTheme } from '../../theme/theme';
 import Button from '../Component/BottomButtonComponent';
+import chickImage from '../../avatars/chick.png';
+import foxImage from '../../avatars/fox.png';
+import frogImage from '../../avatars/frog.png';
+import octopusImage from '../../avatars/octopus.png';
+import pigImage from '../../avatars/pig.png';
+import whaleImage from '../../avatars/whale.png';
 
 type Props = NativeStackScreenProps<RootStackParamList, 'JoinHousehold'>;
 
 export default function JoinHouseholdScreen({ navigation }: Props) {
   const [name, setName] = useState('');
   const [code, setCode] = useState('');
+  const [selectedAvatar, setSelectedAvatar] = useState('');
 
+  const availableAvatars = ['chick', 'fox', 'frog', 'octopus', 'pig', 'whale'];
+
+  const handleAvatarSelection = (avatar) => {
+    if (availableAvatars.includes(avatar)) {
+      setSelectedAvatar(avatar);
+  
+      availableAvatars.splice(availableAvatars.indexOf(avatar), 1);
+    } else {
+      <Text>Avataren är inte tillgänglig!</Text>
+    }
+  };
+  
+  
   const placeholderStyle = {
     width: 300,
     height: 40,
     backgroundColor: ProjectTheme.inputBackground,
     borderRadius: ProjectTheme.borderRadius.medium,
     paddingLeft: 10,
-    marginBottom: 20,
+    marginBottom: 10,
     color: ProjectTheme.colors.textcolor,
     elevation: ProjectTheme.elevation.small,
   };
+
+  const saveUserData = () => {
+    const newProfile = {
+      name: name,
+      code: code,
+      avatar: selectedAvatar,
+    }
+    console.log('New profile created: ', newProfile);
+  };
+
 
   return (
     <View
@@ -34,20 +65,63 @@ export default function JoinHouseholdScreen({ navigation }: Props) {
     >
       <View style={{}}>
         <TextInput
-          style={placeholderStyle} // Apply the placeholderStyle here
-          placeholder="Namn"
+          style={placeholderStyle}
+          placeholder="Ditt namn"
           placeholderTextColor={ProjectTheme.inputPlaceholderColor}
           onChangeText={(text) => setName(text)}
           value={name}
         />
 
         <TextInput
-          style={placeholderStyle} // Apply the placeholderStyle here
+          style={placeholderStyle}
           placeholder="Hushålls kod"
           placeholderTextColor={ProjectTheme.inputPlaceholderColor}
           onChangeText={(text) => setCode(text)}
           value={code}
         />
+      </View>
+
+      <View
+        style={{
+          flexDirection: 'column',
+          alignItems: 'center',
+          marginLeft: 5,
+        }}
+      >
+        {availableAvatars.map((avatar) => (
+          <View key={avatar} style={{ flexDirection: 'row', alignItems: 'center' }}>
+             <View style={{ padding: 10, borderRadius: 30, backgroundColor: 'white' }}>
+            <Checkbox
+              status={selectedAvatar === avatar ? 'checked' : 'unchecked'}
+              onPress={() => handleAvatarSelection(avatar)}
+              color='black'
+            />
+            </View>
+            <Image
+              source={
+                avatar === 'octopus'
+                  ? octopusImage
+                  : avatar === 'frog'
+                  ? frogImage
+                  : avatar === 'fox'
+                  ? foxImage
+                  : avatar === 'chick'
+                  ? chickImage
+                  : avatar === 'whale'
+                  ? whaleImage
+                  : pigImage
+              }
+              style={{
+                width: 50,
+                height: 50,
+                borderRadius: 25,
+                borderWidth: selectedAvatar === avatar ? 2 : 0,
+                borderColor: ProjectTheme.colors.primary,
+                marginLeft: 260
+              }}
+            />
+          </View>
+        ))}
       </View>
 
       <View
@@ -60,14 +134,15 @@ export default function JoinHouseholdScreen({ navigation }: Props) {
         <Button
           title="Spara"
           onPress={() => {
-            navigation.navigate('MyHouseholds')
+            saveUserData();
+            navigation.navigate('MyHouseholds');
           }}
         />
 
         <Button
           title="Stäng"
           onPress={() => {
-            navigation.navigate('MyHouseholds')
+            navigation.navigate('MyHouseholds');
           }}
         />
       </View>
