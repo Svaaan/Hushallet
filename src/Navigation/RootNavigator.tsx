ffdbdfda156c4b84e4b27c44d58d45f81bf0f234
Honer Jamal <Honerjamal@gmail.com>
2023-10-25 12:25:09 +0200

    hejhej

--- src/Navigation/RootNavigator.tsx
+++ src/Navigation/RootNavigator.tsx
@@ -3,9 +3,7 @@ import React from 'react';
 import CreateAccountScreen from '../Screen/CreateAccountScreen';
 import CreateHouseholdScreen from '../Screen/CreateHouseholdScreen';
 import CreateTaskScreen from '../Screen/CreateTaskScreen';
-import EditTaskScreen from '../Screen/EditTaskScreen';
 import HomeScreen from '../Screen/HomeScreen';
-import JoinHouseholdScreen from '../Screen/JoinHouseholdScreen';
 import LoginScreen from '../Screen/LoginScreen';
 import MyHouseholdsScreen from '../Screen/MyHouseholdsScreen';
 import ProfileScreen from '../Screen/ProfileScreen';
@@ -13,10 +11,8 @@ import ProfileSettingScreen from '../Screen/ProfileSettingScreen';
 import StartScreen from '../Screen/StartScreen';
 import TaskDetailsScreen from '../Screen/TaskDetailsScreen';
 import HouseholdSwipeNavigator from './HouseholdSwipeNavigator';
-import JoinHouseholdScreen from '../Screen/JoinHouseholdScreen';
-import DisplayUsersScreen from '../Screen/DisplayUsersScreen';
 import EditHouseholdScreen from '../Screen/EditHouseholdScreen';
-
+import JoinHouseholdScreen from '../Screen/JoinHouseholdScreen';
 
 export type RootStackParamList = {
   Home: undefined;
@@ -30,17 +26,15 @@ export type RootStackParamList = {
   ProfileSettings: undefined;
   Household: undefined;
   CreateTask: undefined;
-  EditTask: undefined;
-  JoinHousehold: undefined;
-  EditHousehold: undefined;
-  Users: undefined;
+  JoinHousehold: undefined
+  EditHousehold: undefined
 };
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
 export default function RootNavigator() {
   return (
-    <Stack.Navigator initialRouteName="Household">
+    <Stack.Navigator initialRouteName="Start">
       <Stack.Screen name="Start" component={StartScreen} />
       <Stack.Screen name="Home" component={HomeScreen} />
       <Stack.Screen name="CreateAccount" component={CreateAccountScreen} />
@@ -51,16 +45,13 @@ export default function RootNavigator() {
       <Stack.Screen name="JoinHousehold" component={JoinHouseholdScreen} />
       <Stack.Screen name="MyHouseholds" component={MyHouseholdsScreen} />
       <Stack.Screen name="ProfileSettings" component={ProfileSettingScreen} />
+      <Stack.Screen name="EditHousehold" component={EditHouseholdScreen} />
 
       <Stack.Screen name="Household" component={HouseholdSwipeNavigator} />
-      <Stack.Screen name="EditHousehold" component={EditHouseholdScreen} />
-      <Stack.Screen name="Users" component={DisplayUsersScreen} />
-
 
       <Stack.Group screenOptions={{ presentation: 'modal' }}>
         <Stack.Screen name="TaskDetails" component={TaskDetailsScreen} />
         <Stack.Screen name="CreateTask" component={CreateTaskScreen} />
-        <Stack.Screen name="EditTask" component={EditTaskScreen} />
       </Stack.Group>
     </Stack.Navigator>
   );
---
+++ src/Screen/JoinHouseholdScreen.tsx
@@ -0,0 +1,135 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { NativeStackScreenProps } from '@react-navigation/native-stack';
+import React, { useState } from 'react';
+import { Image, TextInput, View, Text } from 'react-native';
+import { Checkbox } from 'react-native-paper';
+import { ProjectTheme } from '../../theme/theme';
+import Button from '../Component/BottomButtonComponent';
+import ChooseEmoji from '../Component/ChooseEmoji';
+import { RootStackParamList } from '../Navigation/RootNavigator';
+
+type Props = NativeStackScreenProps<RootStackParamList, 'JoinHousehold'>;
+
+export default function JoinHouseholdScreen({ navigation }: Props) {
+  const [name, setName] = useState('');
+  const [code, setCode] = useState('');
+  const [selectedAvatar, setSelectedAvatar] = useState('');
+
+  const handleAvatarSelection = (avatar: string) => {
+    setSelectedAvatar(avatar);
+  };
+
+  const placeholderStyle = {
+    width: 300,
+    height: 40,
+    backgroundColor: ProjectTheme.inputBackground,
+    borderRadius: ProjectTheme.borderRadius.medium,
+    paddingLeft: 10,
+    marginBottom: 10,
+    color: ProjectTheme.colors.textcolor,
+    elevation: ProjectTheme.elevation.small,
+  };
+
+  const saveUserData = async () => {
+    const newProfile = {
+      name: name,
+      code: code,
+      avatar: selectedAvatar,
+    };
+    try {
+      const profileString = JSON.stringify(newProfile);
+
+      await AsyncStorage.setItem('userProfile', profileString);
+      console.log('New profile saved to AsyncStorage: ', newProfile);
+    } catch (error) {
+      console.log('Error saving profile to AsyncStorage: ', error);
+    }
+  };
+
+  const retrieveUserData = async () => {
+    try {
+      const profileString = await AsyncStorage.getItem('userProfile');
+      if (profileString !== null) {
+        const userProfile = JSON.parse(profileString);
+        console.log('Retrieved profile from AsyncStorage: ', userProfile);
+      } else {
+        console.log('No profile found in AsyncStorage.');
+      }
+    } catch (error) {
+      console.log('Error retrieving profile from AsyncStorage: ', error);
+    }
+  };
+
+  return (
+    <View
+      style={{
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        backgroundColor: ProjectTheme.colors.background,
+        paddingTop: 200,
+      }}
+    >
+      <View>
+        <TextInput
+          style={placeholderStyle}
+          placeholder="Ditt namn"
+          placeholderTextColor={ProjectTheme.inputPlaceholderColor}
+          onChangeText={(text) => setName(text)}
+          value={name}
+        />
+
+        <TextInput
+          style={placeholderStyle}
+          placeholder="Hushålls kod"
+          placeholderTextColor={ProjectTheme.inputPlaceholderColor}
+          onChangeText={(text) => setCode(text)}
+          value={code}
+        />
+      </View>
+
+      <View
+        style={{
+          flexDirection: 'column',
+          alignItems: 'center',
+          marginLeft: 5,
+        }}
+      >
+        <ChooseEmoji
+          selectedEmoji={selectedAvatar}
+          onSelectEmoji={handleAvatarSelection}
+        />
+      </View>
+
+      <View
+        style={{
+          flexDirection: 'row',
+          justifyContent: 'space-evenly',
+          width: '100%',
+        }}
+      >
+        <Button
+          title="Spara"
+          onPress={() => {
+            saveUserData();
+            navigation.navigate('MyHouseholds');
+          }}
+        />
+
+        <Button
+          title="Stäng"
+          onPress={() => {
+            navigation.navigate('MyHouseholds');
+          }}
+        />
+        {/* Debugging för att kontroller aatt async storage funkar korrekt */}
+        {/* <Button
+          title="Hämta"
+          onPress={() => {
+            retrieveUserData();
+          }}
+        /> */}
+      </View>
+    </View>
+  );
+}
