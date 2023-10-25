3bde9ba4316edae2edeb08636e4fab943e192319
Svaaan <daniel_svan1@hotmail.com>
2023-10-25 22:57:33 +0200

    profile screen gets id from parameter and the profile is presented

--- src/Screen/ProfileScreen.tsx
+++ src/Screen/ProfileScreen.tsx
@@ -1,14 +1,21 @@
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-
 import { RootStackParamList } from '../Navigation/RootNavigator';
+import { useProfileContext } from '../Context/ProfileContext';
 
 type Props = NativeStackScreenProps<RootStackParamList, 'Profile'>;
 
-export default function ProfileScreen({ navigation }: Props) {
+export default function ProfileScreen({ navigation, route }: Props) {
+  const { getProfileById } = useProfileContext();
+
+  const profileId = route.params.userId;
+  const profile = getProfileById(profileId);
+
   return (
     <View style={styles.container}>
+      <Text>User ID: {profile?.id}</Text>
+      <Text>Username: {profile?.name}</Text>
       <Text>Profile Screen</Text>
     </View>
   );
