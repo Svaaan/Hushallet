2a7115681be901f21e45caed746b27c3707a45c9
Svaaan <daniel_svan1@hotmail.com>
2023-10-25 22:41:53 +0200

    removed userid from account

--- data/mockedAccount.ts
+++ data/mockedAccount.ts
@@ -2,7 +2,6 @@ interface Account {
   id: number;
   username: string;
   password: string;
-  userId: number;
 }
 
 const mockedAccounts: Account[] = [
@@ -10,25 +9,21 @@ const mockedAccounts: Account[] = [
     id: 1,
     username: 'JohnAccount',
     password: 'password123',
-    userId: 1,
   },
   {
     id: 2,
-    username: 'AliceAccount',
-    password: 'alicePass',
-    userId: 2,
+    username: 'Alice',
+    password: '123',
   },
   {
     id: 3,
     username: 'BobAccount',
     password: 'bobPassword',
-    userId: 3,
   },
   {
     id: 4,
     username: 'MayaAccount',
     password: 'mayaPass',
-    userId: 4,
   },
 ];
 
