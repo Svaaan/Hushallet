828377045e125699511e001c40e2829c95b38eef
HonerJamal <91282790+HonerJamal@users.noreply.github.com>
2023-10-25 12:56:03 +0200

    navigation

--- src/Navigation/RootNavigator.tsx
+++ src/Navigation/RootNavigator.tsx
@@ -6,13 +6,12 @@ import CreateTaskScreen from '../Screen/CreateTaskScreen';
 import HomeScreen from '../Screen/HomeScreen';
 import LoginScreen from '../Screen/LoginScreen';
 import MyHouseholdsScreen from '../Screen/MyHouseholdsScreen';
-import ProfileScreen from '../Screen/ProfileScreen';
 import ProfileSettingScreen from '../Screen/ProfileSettingScreen';
 import StartScreen from '../Screen/StartScreen';
 import TaskDetailsScreen from '../Screen/TaskDetailsScreen';
 import HouseholdSwipeNavigator from './HouseholdSwipeNavigator';
-import EditHouseholdScreen from '../Screen/EditHouseholdScreen';
 import JoinHouseholdScreen from '../Screen/JoinHouseholdScreen';
+import HouseProfileScreen from '../Screen/HouseProfileScreen';
 
 export type RootStackParamList = {
   Home: undefined;
@@ -20,14 +19,13 @@ export type RootStackParamList = {
   CreateHousehold: undefined;
   MyHouseholds: undefined;
   CreateAccount: undefined;
-  Profile: undefined;
+  HouseProfile: undefined;
   Login: undefined;
   TaskDetails: undefined;
   ProfileSettings: undefined;
   Household: undefined;
   CreateTask: undefined;
   JoinHousehold: undefined
-  EditHousehold: undefined
 };
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
@@ -40,12 +38,11 @@ export default function RootNavigator() {
       <Stack.Screen name="CreateAccount" component={CreateAccountScreen} />
       <Stack.Screen name="Login" component={LoginScreen} />
 
-      <Stack.Screen name="Profile" component={ProfileScreen} />
+      <Stack.Screen name="HouseProfile" component={HouseProfileScreen} />
       <Stack.Screen name="CreateHousehold" component={CreateHouseholdScreen} />
       <Stack.Screen name="JoinHousehold" component={JoinHouseholdScreen} />
       <Stack.Screen name="MyHouseholds" component={MyHouseholdsScreen} />
       <Stack.Screen name="ProfileSettings" component={ProfileSettingScreen} />
-      <Stack.Screen name="EditHousehold" component={EditHouseholdScreen} />
 
       <Stack.Screen name="Household" component={HouseholdSwipeNavigator} />
 
--- src/Screen/HouseProfileScreen.tsx
+++ src/Screen/HouseProfileScreen.tsx
@@ -1,10 +1,9 @@
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-
 import { RootStackParamList } from '../Navigation/RootNavigator';
 
-type Props = NativeStackScreenProps<RootStackParamList, 'Profile'>;
+type Props = NativeStackScreenProps<RootStackParamList, 'HouseProfile'>;
 
 export default function ProfileScreen({ navigation }: Props) {
   return (
