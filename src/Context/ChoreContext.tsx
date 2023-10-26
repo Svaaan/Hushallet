344b5f955603f031aa02bd344e6c74cb91e9dcb5
Nawzad75 <113518854+Nawzad75@users.noreply.github.com>
2023-10-26 10:25:17 +0200

    1

---
+++ src/Context/ChoreContext.tsx
@@ -0,0 +1,31 @@
+import React, { ReactNode, createContext, useContext, useState } from 'react';
+import { Chore } from '../../data/mockedChores';
+
+type ChoreContextType = {
+  chores: Chore[];
+  setChore: (chores: Chore[]) => void;
+}
+
+const ChorContext = createContext<ChoreContextType | undefined>(undefined);
+
+export const TaskProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
+  const [chores, setChores] = useState<Chore | null>(null);
+
+  return (
+    <ChorContext.Provider value={{
+      chores, setChore(chores) {
+
+      },
+    }}>{children}</ChorContext.Provider>
+  );
+};
+
+export const useTask = () => {
+  const context = useContext(ChorContext);
+
+  if (context === undefined) {
+    throw new Error('useTask must be used within a TaskProvider');
+  }
+
+  return context;
+};
