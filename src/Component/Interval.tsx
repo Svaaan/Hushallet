0d0f44836724399d0fd893744d43d8f644d26067
Nawzad75 <113518854+Nawzad75@users.noreply.github.com>
2023-10-28 05:00:20 +0200

    nästanklart

--- src/Component/Interval.tsx
+++ src/Component/Interval.tsx
@@ -12,35 +12,35 @@ const Intervals = () => {
     setNr(number);
     console.log('Valt nummer:', number);
     setScrollViewOpen(false); // Stäng ScrollView efter val
-    const nameStyle = {
-      width: '100%',
-      height: 40,
-      backgroundColor: ProjectTheme.inputBackground,
-      borderRadius: ProjectTheme.borderRadius.medium,
-      paddingLeft: 10,
-      paddingRight: 10,
-      paddingTop: 10,
-      marginBottom: 20,
-      color: ProjectTheme.colors.textcolor,
-      elevation: ProjectTheme.elevation.small,
-    };
   };
+
   return (
     <View
       style={{
         flexDirection: 'row',
         width: '100%',
-        height: 70,
+        height: 60,
         backgroundColor: ProjectTheme.inputBackground,
         borderRadius: ProjectTheme.borderRadius.medium,
         paddingLeft: 10,
         paddingRight: 10,
-        paddingTop: 25,
+        paddingTop: 18,
         elevation: ProjectTheme.elevation.small,
       }}
     >
-      <View>
-        <TouchableOpacity onPress={() => setScrollViewOpen(!scrollViewOpen)}>
+      {scrollViewOpen ? (
+        <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+          {numbers.map((number) => (
+            <TouchableOpacity
+              key={number}
+              onPress={() => handleNumberSelection(number)}
+            >
+              <Text style={{ margin: 8 }}>{number}</Text>
+            </TouchableOpacity>
+          ))}
+        </ScrollView>
+      ) : (
+        <TouchableOpacity onPress={() => setScrollViewOpen(true)}>
           <View style={{ flexDirection: 'row' }}>
             <Text style={{}}>Återkommer</Text>
             <Text style={{ paddingLeft: 200 }}>var</Text>
@@ -48,19 +48,7 @@ const Intervals = () => {
             <Text style={{ paddingLeft: 10 }}>dag</Text>
           </View>
         </TouchableOpacity>
-        {scrollViewOpen ? (
-          <ScrollView horizontal showsHorizontalScrollIndicator={false}>
-            {numbers.map((number) => (
-              <TouchableOpacity
-                key={number}
-                onPress={() => handleNumberSelection(number)}
-              >
-                <Text style={{ margin: 10 }}>{number}</Text>
-              </TouchableOpacity>
-            ))}
-          </ScrollView>
-        ) : null}
-      </View>
+      )}
     </View>
   );
 };
